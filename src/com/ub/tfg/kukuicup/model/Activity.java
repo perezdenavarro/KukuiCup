f9ff618af39dc048b3b193296e048de5c972fe5b
perezdenavarro <perezdenavarro@gmail.com>
2016-05-25 00:53:51 +0200

    Activity class created

---
+++ src/com/ub/tfg/kukuicup/model/Activity.java
@@ -0,0 +1,38 @@
+package com.ub.tfg.kukuicup.model;
+
+public class Activity {
+	private int id;
+	private String name;
+	private String description;
+	
+	public Activity() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}

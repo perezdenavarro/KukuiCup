5f641d6f7a7cc93549584c5abba5575525cd7145
perezdenavarro <perezdenavarro@gmail.com>
2016-05-29 20:53:55 +0200

    AlertDialog on blocked levels

--- src/com/ub/tfg/kukuicup/vista/MainActivity.java
+++ src/com/ub/tfg/kukuicup/vista/MainActivity.java
@@ -4,11 +4,14 @@ import com.ub.tfg.kukuicup.R;
 import com.ub.tfg.kukuicup.admin.vista.MenuAdminActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -18,6 +21,7 @@ public class MainActivity extends Activity {
 	private ImageButton level1Btn;
 	private ImageButton level2Btn;
 	private ImageButton level3Btn;
+	private AlertDialog alertDialog;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,6 +33,25 @@ public class MainActivity extends Activity {
 		level2Btn = (ImageButton) findViewById(R.id.level2Btn);
 		level3Btn = (ImageButton) findViewById(R.id.level3Btn);
 		
+		alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+		
+		// Setting Dialog Title
+        alertDialog.setTitle("Level blocked");
+ 
+        // Setting Dialog Message
+        alertDialog.setMessage("You need to complete the previous level to continue");
+ 
+        // Setting Icon to Dialog
+        alertDialog.setIcon(R.drawable.ic_blocked_level);
+ 
+        // Setting OK Button
+        alertDialog.setButton2("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                // Write your code here to execute after dialog closed
+                Toast.makeText(getApplicationContext(), "You can do it!", Toast.LENGTH_SHORT).show();
+                }
+        });
+		
 		level1Btn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				Intent intent = new Intent(ctxt, MenuActivity.class);
@@ -38,15 +61,17 @@ public class MainActivity extends Activity {
 		
 		level2Btn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				Intent intent = new Intent(ctxt, MenuActivity.class);
-				startActivity(intent);
+				alertDialog.show();
+//				Intent intent = new Intent(ctxt, MenuActivity.class);
+//				startActivity(intent);
 			}
 		});
 		
 		level3Btn.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				Intent intent = new Intent(ctxt, MenuAdminActivity.class);
-				startActivity(intent);
+				alertDialog.show();
+//				Intent intent = new Intent(ctxt, MenuAdminActivity.class);
+//				startActivity(intent);
 			}
 		});
 	}

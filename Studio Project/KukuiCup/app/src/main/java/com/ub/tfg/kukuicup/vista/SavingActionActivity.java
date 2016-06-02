e2d792ecc0c942196a577aaa414e32070e69623c
perezdenavarro <perezdenavarro@gmail.com>
2016-06-02 13:04:49 +0200

    Video layout and EnergyChallenge layout added

---
+++ Studio Project/KukuiCup/app/src/main/java/com/ub/tfg/kukuicup/vista/SavingActionActivity.java
@@ -0,0 +1,16 @@
+package com.ub.tfg.kukuicup.vista;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ub.tfg.kukuicup.R;
+
+/**
+ * Created by Juanmi on 02/06/2016.
+ */
+public class SavingActionActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.saving_action);
+    }
+}
